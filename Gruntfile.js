64d569f0fdfc8aca63efa59990b2a0a4c308bef7
felipe <130618161+fa9958189@users.noreply.github.com>
2025-04-28 16:59:29 -0300

    Organiza src, remove maps.less e ajusta imports

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,23 @@
 module.exports = function(grunt) {
-    // 1) Inicializa a configuração
-    grunt.initConfig({
-      // 2) Compilação do LESS
-      less: {
-        dev: {
-          files: {
-            // destino        origem
-            'dist/styles.css': 'src/main.less'
-          }
-        }
-      },
-      // 3) Compressão de JS
-      uglify: {
-        prod: {
-          files: {
-            // destino         origem (pode ser array ou wildcard)
-            'dist/app.min.js': ['src/**/*.js']
-          }
+  grunt.initConfig({
+    less: {
+      dev: {
+        files: {
+          'dist/styles.css': 'src/main.less'
         }
       }
-    });
-  
-    // Carrega os plugins
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-  
-    // Tarefa padrão: basta rodar `grunt` para fazer tudo
-    grunt.registerTask('default', ['less:dev', 'uglify:prod']);
-  };
-  
+    },
+    uglify: {
+      prod: {
+        files: {
+          'dist/app.min.js': ['src/**/*.js']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['less:dev', 'uglify:prod']);
+};
